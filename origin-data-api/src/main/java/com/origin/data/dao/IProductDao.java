d94be9cce4a5be046b28f9dad2652f4d63849921
q01201203 <595458920@qq.com>
2017-05-11 17:06:29 +0800

    delete product

--- origin-data-api/src/main/java/com/origin/data/dao/IProductDao.java
+++
@@ -1,6 +0,0 @@
-package com.origin.data.dao;
-
-import java.io.Serializable;
-
-public interface IProductDao<T, PK extends Serializable> extends IBaseDao<T, PK> {
-}
--- origin-data/origin-mybatis/src/main/java/com/origin/data/mybatis/dao/ProductDao.java
+++
@@ -1,7 +0,0 @@
-package com.origin.data.mybatis.dao;
-
-import java.io.Serializable;
-import com.origin.data.dao.IProductDao;
-
-public interface ProductDao<T, PK extends Serializable> extends IProductDao<T, PK> {
-}
